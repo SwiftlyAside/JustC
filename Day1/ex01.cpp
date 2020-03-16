a659ef9a3726f3f4cb53e4482f1108b07f656a1f
Side <ivecis@live.co.kr>
2020-03-16 15:02:08 +0900

    200316

---
+++ Day1/ex01.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+// 주석
+
+/*
+블록주석
+프로그램의 실행과는 상관없이
+설명을 적는 곳
+*/
+int global_variable;
+
+int main() {
+	/*
+	printf("이름 : 홍길동\n");
+	printf("나이 : 16\n");
+	printf("주소 : 산골짜기\n");
+	
+	printf("\t<(^. .^)>\n");
+	printf("\t(\"(00)\")\n\n");
+	printf("\t배고프다 애용~");
+	
+	printf("%d + %d = %d\n", 5, 12, 5 + 12);
+	printf("%d + %d = \n", 5, 12, 5 + 12);
+	printf("%d + %d = %d\n", 5, 12);
+
+	printf("실수형 상수 : %f \n", 123.456);
+	printf("지수형 상수 : %d \n", 123.456);
+	printf("실수형 상수 : %f \n", 123);
+
+	printf("문자 %c의 ASCII 코드 값음 %d이다.\n", 'a', 'a');
+	printf("ASCII 코드 값이 %d인 문자는 %c 이다.\n", 98, 98);
+
+	printf("%s\n", "This is a string.");
+	*/
+	printf("======================================\n");
+	printf("%s\t\t%s\t\t%s\n", "이름", "나이", "주소");
+	printf("======================================\n");
+	printf("%s\t\t%d\t\t%s\n", "홍길동", 20, "산골짜기");
+	printf("%s\t\t%d\t\t%s\n", "이순신", 28, "바다");
+
+}
